6fcd2a2e0fa3b91e3820a5af61f693a5950dc7e1
Matthew Kay <mkay@bluetreegroup.co.uk>
2021-06-11 08:58:50 +0100

    Fix 'products' element not found while parsing XML report warnings

--- source.js
+++ source.js
@@ -929,7 +929,7 @@ function createLayoutReport(report, layout, job) {
 
 	// Remove products that are not placed into this layout
 	if (edited) {
-		var products = childElement(root, "products");
+		var products = childElement(root, "products2");
 			if (products != null) {
 			var toRemove = [];
 			var children = products.getChildNodes();
